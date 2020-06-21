ccb59df9538314b4317ae0fc40a4128cf46ebb7e
Arshitha2020 <61614252+Arshitha2020@users.noreply.github.com>
2020-06-21 14:53:13 +0530

    Create header.php
    header for the php part of the project

---
+++ header.php
@@ -0,0 +1,38 @@
+<head>
+	<title>Arshithas Page</title>
+	 <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+
+    <style type="text/css">
+    	.brand{
+    		background:  #9e9d24 !important;/*important is to override any kind of materialize classes on that aswell*/
+    	}
+    	.brand-text{
+    		color: #9e9d24  !important;
+    	}
+    	form{
+    		max-width: 460px;
+    		margin: 20px auto;
+    		padding: 20px;
+    	}
+    	.book{
+    		width: 100px;
+    		margin: 40px auto -30px;
+    		display: block;
+    		position: relative;
+    		top: -30px;
+
+    	}
+    </style>
+</head>
+<body class=" blue-grey lighten-4"> <!-- this body tag will close in footer, since that comes at the end of each page-->
+	<nav class="white z-depth-0">
+		<div class="container">
+			<a href="#" class="brand-logo brand-text">Biblio Book Club</a>
+			<ul id="nav-mobile" class="right hide-on-small-and-down">
+				<li><a href="add.php" class="btn brand z-depth-0">Add a Book</a></li>				
+			</ul>
+		</div>
+
+	</nav>
